4a612071d63dcc2ba8b1dd361c3478e23c43b960
LongTran8102 <longtpp0801@gmail.com>
2023-05-10 22:19:30 +0700

    Long

---
+++ TablesideOrdering/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace TablesideOrdering.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public string Pic { get; set; }
+    }
+}
