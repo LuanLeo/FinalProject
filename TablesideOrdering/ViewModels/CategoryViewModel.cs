deafe4d263232a41704f5874e6151c7445a91e10
LuanLeo <luandtgcs200115@fpt.edu.vn>
2023-05-11 22:23:19 +0700

    category

---
+++ TablesideOrdering/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using TablesideOrdering.Models;
+
+namespace TablesideOrdering.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public Category Category { get; set; }
+    }
+}
