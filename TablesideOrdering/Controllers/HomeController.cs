ba8731873c25bd5845c0e77e742d95c6a43adbb8
LongTran8102 <longtpp0801@gmail.com>
2023-05-17 22:43:58 +0700

    User homepage

--- TablesideOrdering/Controllers/HomeController.cs
+++ TablesideOrdering/Controllers/HomeController.cs
@@ -1,21 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using TablesideOrdering.Data;
 using TablesideOrdering.Models;
+using TablesideOrdering.ViewModels;
 
 namespace TablesideOrdering.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var Homedata = new HomeViewModel();
+            var product = (from products in _context.Products
+                           select new Product
+                           {
+                               ProductId = products.ProductId,
+                               Price = products.Price,
+                               Description = products.Description,
+                               Name = products.Name,
+                               Pic = products.Pic,
+                               CategoryId = products.CategoryId,
+                           });
+            var cat = (from categories in _context.Categories
+                       select new Category
+                       {
+                           CategoryId = categories.CategoryId,
+                           CategoryName = categories.CategoryName,
+                       });
+            Homedata.Category = cat;
+            Homedata.Product = product;
+            return View(Homedata);
         }
 
         public IActionResult Privacy()
---
+++ TablesideOrdering/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using TablesideOrdering.Models;
+
+namespace TablesideOrdering.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IQueryable<Category> Category { get; set; }
+        public IQueryable<Product> Product { get; set; }
+    }
+}
