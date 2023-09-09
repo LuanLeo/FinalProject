2e5ebf3908544b4169a60871442e3dfe18f8704d
LongTran8102 <longtpp0801@gmail.com>
2023-09-09 10:55:14 +0700

    chat bug

--- TablesideOrdering/wwwroot/js/chat.js
+++ TablesideOrdering/wwwroot/js/chat.js
@@ -6,10 +6,30 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
-    var p = document.createElement("p");
-
-    p.textContent = `${user} says ${message}`;
-    document.getElementById("messagesList").appendChild(p);
+    var doc = $('<div/>');
+    //var datetime = currentdate.getHours() + ":"+ currentdate.getMinutes();
+    doc.append(`<div class="chat-message clearfix">`);
+    doc.append('<img src="/Logo/Avatar/defaultavatar.png" alt="" width="32" height="32">');
+    doc.append('<div class="chat-message-content clearfix">');
+    doc.append(`<span class="chat-time"></span>`);
+    doc.append(`<h5 id="${user}"></h5>`);
+    doc.append(`<p >${message}</p>`);
+    doc.append('</div> <!-- end chat-message-content -->');
+    doc.append('<hr>');
+    $('#chatbox').append(doc);
+   
+    
+    /*tr = $('<tr/>');
+    tr.append(`<td>${(index + 1)}</td>`);
+    tr.append(`<td>${(order.orderDate)}</td>`);
+    tr.append(`<td>${(order.orderPrice)}</td>`);
+    tr.append(`<td>${(order.productQuantity)}</td>`);
+    tr.append(`<td>${(order.phoneNumber)}</td>`);
+    tr.append(`<td>${(order.cusName)}</td>`);
+    tr.append(`<td>${(order.tableNo)}</td>`);
+    tr.append(`<td><button type="button" class="btn btn-warning m-1" onclick="location.href='../Staff/Order/Details?id=${(order.orderId)}'">Details</button></td>`);
+    $('#EatInOrder').append(tr);*/
+    
 
 });
 
