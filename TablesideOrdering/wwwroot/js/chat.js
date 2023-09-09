ddf4675703d366edecc8eb94ba94fba14e49c6e1
Đinh Luân <luandtgcs200115@fpt.edu.vn>
2023-09-09 21:23:41 +0700

    set role chat js

--- TablesideOrdering/wwwroot/js/chat.js
+++ TablesideOrdering/wwwroot/js/chat.js
@@ -5,30 +5,11 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 //Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
 
-connection.on("ReceiveMessage", function (user, message) {
-    if (message != null) {
-        var currentdate = new Date();
-        var datetime = currentdate.getDay() + "/" + currentdate.getMonth() + "/" + currentdate.getFullYear() + " "
-            + currentdate.getHours() + ":" + currentdate.getMinutes();
-
-        var doc = $('<div class="chat-message clearfix"/>');
-        doc.append('    <img src="/Logo/Avatar/defaultavatar.png" alt="" width="32" height="32">');
-        doc.append('    <div class="chat-message-content clearfix">'
-            + <span class="chat-time">${datetime}</span>
-            + <h5>Table - ${user}</h5>
-            + <p>${message}</p>
-            +</div >);
-        doc.append(<hr>);
-            $('#chatbox').append(doc);
-    }
-});
-
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
 });
-
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
@@ -41,5 +22,42 @@ document.getElementById("sendButton").addEventListener("click", function (event)
         });
     }
     event.preventDefault();
+});
+
+connection.on("ReceiveMessage", function (user, message) {
+    if (message != null) {
+        if (user != "Admin") {
+            var currentdate = new Date();
+            var datetime = currentdate.getDay() + "/" + currentdate.getMonth() + "/" + currentdate.getFullYear() + " "
+                + currentdate.getHours() + ":" + currentdate.getMinutes();
+
+            var doc = $('<div class="chat-message clearfix"/>');
+            doc.append('    <img src="/Logo/Avatar/defaultavatar.png" alt="" width="32" height="32">');
+            doc.append('    <div class="chat-message-content clearfix">'
+                + `<span class="chat-time">${datetime}</span>`
+                + `<h5>Table - ${user}</h5>`
+                + `<p>${message}</p>`
+                + `</div>`
+                + `</div>'`)
+            doc.append('<hr>');
+            $('#chatbox').append(doc);
+        }
+        else {
+            var currentdate = new Date();
+            var datetime = currentdate.getDay() + "/" + currentdate.getMonth() + "/" + currentdate.getFullYear() + " "
+                + currentdate.getHours() + ":" + currentdate.getMinutes();
+
+            var doc = $('<div class="chat-message clearfix"/>');
+            doc.append('    <img src="/Logo/Avatar/user.png" alt="" width="32" height="32">');
+            doc.append('    <div class="chat-message-content clearfix">'
+                + `<span class="chat-time">${datetime}</span>`
+                + `<h5>Table - ${user}</h5>`
+                + `<p>${message}</p>`
+                + `</div>`
+                + `</div>'`)
+            doc.append('<hr>');
+            $('#chatbox').append(doc);
+        }
+    }
 });
 
